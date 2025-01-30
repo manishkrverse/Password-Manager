38c84764d25a629dfd4741e7ed8baaf98a8e5734
MANISH KUMAR <manishkumarverse@gmail.com>
2025-01-30 19:08:24 +0530

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,12 +13,12 @@ const Navbar = () => {
                     
                     </div>
                 
-{/*                 <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
-                    <a className='flex items-center' href=""> */}
+                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
+{/*                     <a className='flex items-center' href="">
 
-{/*                     <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
-                    <span className='font-bold px-2'>GitHub</span> */}
-                    </a>
+                    <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
+                    <span className='font-bold px-2'>GitHub</span>
+                    </a> */}
                     
                 </button>
             </div>
